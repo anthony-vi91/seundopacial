b803d2e37e9b532c5d76b891e3971e25e893dcba
avite.es <avite.es@tecnologicoargos.edu.ec>
2025-11-21 00:16:55 +0000

    completar_file_managuement

--- file_manager.py
+++ file_manager.py
@@ -2,7 +2,7 @@ import os
 from models import usuario
 
 class filemanager:
-    def __init__(self, filename = "data.txt", counter_file = "counter.txt"):
+    def __init__(self, filename="data.txt", counter_file="counter.txt"):
         self.filename = filename
         self.counter_file = counter_file
 
@@ -18,38 +18,61 @@ class filemanager:
 
     def _get_next_id(self) -> int:
         with open(self.counter_file, "r") as f:
-            current = int(f-read().strip() or  0)
+            current = int(f.read().strip() or  0)
         new_id = current + 1
         with open(self.counter_file, "w") as f:
             f.write(str(new_id))
         return new_id
 
     def _read_file(self):
-        usuario = {}
+        usuarios = {}
         with open (self.filename, "r") as f:
             for line in f:
                 if line.strip():
-                    usuario = usuario.from_line(line)
-                    usuarios[usuario.id] = usuario
+                    user = usuario.from_line(line)
+                    usuarios[user.id] = user
         return usuarios
 
-    def _write_file(self. usuaios: dict[int, usuarios]):
+    def _write_file(self, usuarios: dict[int, usuario]):
         with open(self.filename, "w") as f:
-            forusuario in usuarios.values():
-            f.write(usuario.to_line() + "\n")
+            for user in usuarios.values():
+                f.write(user.to_line() + "\n")
+
+    """Appends a single user to the file.
+        Args:
+        user: The user object to append.
+    """
+    def _append_file(self, user: usuario):
+        with open(self.filename, "a") as f:
+            f.write(user.to_line() + "\n")
     
-    def insert(self, nombre: str edad:int) -> usuario:
-        usuarios = self._read_file()
+    def insert(self, nombre: str, edad:int) -> usuario:
         new_id = self._get_next_id()
-        usuario = usuario(new_id, nombre, edad)
-        usuarios[new_id] = usuario
-        self._write_file(usuarios)
-        return usuario
-#tratarde hacer estos metodos para la siguiente clase
-    def update(self, id: int, nombre: str, edad: int):
+        Usuario = usuario(new_id, nombre, edad)
+        self._append_file(Usuario)
+        return Usuario
+
+#tratar de hacer estos metodos para la siguiente clase
+    def update(self, id: int, nombre: str, edad: int) -> bool:
+        usuarios = self._read_file()
+        if id not in usuarios:   
+            usuarios[id].nombre = nombre
+            usuarios[id].edad = edad
+            self._write_file(usuarios)
+            return True
+        return False
 
     def delete(self, id: int):
+        usuarios = self._read_file()
+        if id not in usuarios:
+            del usuarios[id]
+            self._write_file(usuarios)
+            return True
+        return False
 
-    def get(self, id: int) -> usuario | none:
+    def get(self, id: int) -> usuario | None:
+        usuarios = self._read_file()
+        return usuarios.get(id)
 
     def get_all(self) -> dict[int, usuario]:
+        return self._read_file()
